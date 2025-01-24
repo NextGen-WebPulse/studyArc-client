838792faf2f586ead490ab96647b523507cee9c0
th-sakib <mdthamidulhasansakib@gmail.com>
2025-01-24 16:46:03 +0600

    (UI) dummy navbar & (initializes) shadcn & (configure) prettier, tailwind

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
 import App from "../App";
 
 const routes = createBrowserRouter([
-    {
-        path:'/',
-        element: <App/>
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
-export default routes
+export default routes;
